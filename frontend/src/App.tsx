d6800145c54de8be581a29a4d00a07cfacd01260
sanyuchilas <sanyuchilas@gmail.com>
2023-02-05 15:04:10 +0300

    Add recharts into dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import './App.css';
 
 function App() {
+  const data = [
+    {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+    {name: 'Page B', uv: 300, pv: 500, amt: 2400},
+    {name: 'Page C', uv: 0, pv: 200, amt: 2400},
+    {name: 'Page D', uv: 200, pv: 2400, amt: 2400},
+    {name: 'Page E', uv: 200, pv: 100, amt: 2400},
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineChart width={600} height={300} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
     </div>
   );
 }
