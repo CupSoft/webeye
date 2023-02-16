302ec4830c75cab24264c44b54270949eb4dbe05
sanyuchilas <sanyuchilas@gmail.com>
2023-02-17 00:17:50 +0300

    Fixed some styles

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,23 +1,10 @@
-import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import styles from './MainPage.module.scss'
 
 const MainPage = () => {
-  const data = [
-    {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
-    {name: 'Page B', uv: 300, pv: 500, amt: 2400},
-    {name: 'Page C', uv: 0, pv: 200, amt: 2400},
-    {name: 'Page D', uv: 200, pv: 2400, amt: 2400},
-    {name: 'Page E', uv: 200, pv: 100, amt: 2400},
-  ];
   return (
-    <div>
-      <LineChart width={300} height={300} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="name" />
-        <YAxis />
-      </LineChart>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Мониторинг система</h1>
+      <span className={styles.description}>Наша система отслеживает состояние и доступность информационных ресурсов российских ВУЗов</span>
     </div>
   );
 };
--- frontend/src/pages/SourcePage/SourcePage.tsx
+++ frontend/src/pages/SourcePage/SourcePage.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import styles from './SourcePage.module.scss'
 
 const SourcePage = () => {
   const params = useParams()
+  const data = [
+    {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+    {name: 'Page B', uv: 300, pv: 500, amt: 2400},
+    {name: 'Page C', uv: 0, pv: 200, amt: 2400},
+    {name: 'Page D', uv: 200, pv: 2400, amt: 2400},
+    {name: 'Page E', uv: 200, pv: 100, amt: 2400},
+  ];
+
   return (
     <div>
       {`Source: ${params.id}`}
+      <LineChart width={300} height={300} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
     </div>
   );
 };
