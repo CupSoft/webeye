4546fb3cea36198b3105c81e0d20d66552d9c344
Artem <artem00561@gmail.com>
2023-02-17 02:03:28 +0300

    init: added a postman

---
+++ postman/sender.py
@@ -0,0 +1,24 @@
+import logging
+from asyncio import sleep
+from email.message import EmailMessage
+from os import getenv
+
+import aiosmtplib
+
+
+async def sender(to, event, details):
+    message = EmailMessage()
+    message["From"] = getenv("SMTP_USERNAME")
+    message["To"] = to
+    message["Subject"] = event
+    message.set_content(details)
+    try:
+        await aiosmtplib.send(message,
+                              hostname=getenv("SMTP_HOST"),
+                              port=getenv("SMTP_PORT"),
+                              username=getenv("SMTP_USERNAME"),
+                              password=getenv("SMTP_PASSWORD"),
+                              use_tls=True)
+        logging.info(f"The email was successfully sent to {to}")
+    except Exception as e:
+        logging.error(e)
