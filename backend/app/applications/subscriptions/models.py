2b70a3f80b695fd5b87839686d62553eaa8258a1
Deformater <muravenkog@yandex.ru>
2023-02-18 01:10:49 +0300

    feat(db): refactoring models
    feat(db): create report and social-network report models
    
    fixes issue #9

--- backend/app/applications/subscriptions/models.py
+++ backend/app/applications/subscriptions/models.py
@@ -2,14 +2,11 @@ from tortoise import fields
 
 from app.core.base.base_models import BaseModel
 
-from app.applications.subscriptions.schemas import SubscriptionType
-
 
 class Subscription(BaseModel):
 
     to_telegram = fields.BooleanField(default=False)
     to_email = fields.BooleanField(default=False)
-    notification_level = fields.IntEnumField(SubscriptionType)
     resource: fields.ForeignKeyRelation['Resource'] = fields.ForeignKeyField(
         'models.Resource', related_name='subscriptions', to_field='uuid', on_delete=fields.CASCADE
     )
--- backend/app/applications/subscriptions/schemas.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class SubscriptionType(int, Enum):
-    fucking_slave: 1
-    dungeon_master: 2
