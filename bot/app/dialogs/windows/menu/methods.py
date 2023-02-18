a9a133e2254029620e2bf07004192678392f2296
Artem <artem00561@gmail.com>
2023-02-18 22:39:41 +0300

    Feat: brought beauty

--- bot/app/dialogs/windows/menu/methods.py
+++ bot/app/dialogs/windows/menu/methods.py
@@ -1,14 +1,19 @@
+from random import choice
+
 from aiogram.types import Message
 from aiogram_dialog import DialogManager, DialogProtocol
 
 from app.db.db_user.user_func import User
 from app.dialogs.universal_methods import get_tg_id_from_manager
 
+stickers = ['👍', '👻', '😄', '🧐', '👀', '🌝', '🎫', '🔫', '📌', '📚']
+
 
 async def getter_menu(dialog_manager: DialogManager, **kwargs):
     tg_id = get_tg_id_from_manager(dialog_manager)
     user = await User.get_by_tg(tg_id)
     return {"name": user.name,
+            "sticker": choice(stickers),
             "not_btn_text": "Отключить уведомления" if user.notifications else "Включить уведомления",
             "not_text": "Уведомления включены ✅" if user.notifications else "Уведомления отключены ❌"}
 
