071f45319234ffc51c9fd51af4222c7c84093eef
Artem <artem00561@gmail.com>
2023-02-19 02:09:06 +0300

    feat: Connected to the API

---
+++ checker/app/headers.py
@@ -0,0 +1,1 @@
+base_headers = {"X-Requested-With": "XMLHttpRequest", "Content-Type": "application/json"}
--- checker/app/request_shecker.py
+++ checker/app/request_shecker.py
@@ -2,37 +2,37 @@ import asyncio
 import logging
 import time
 
-from aiohttp import ClientSession
+import aiohttp
 
+from app.headers import base_headers
 from app.models_pdc import Task, Answer
 
 
-async def request_dispatcher(session: ClientSession, tasks: list[Task]) -> list[Answer]:
-    pack = [asyncio.ensure_future(get_request(session, task)) for task in tasks]
-    # TODO: make request from each proxy
-    for task in tasks:
-        if task.method == "get":
+async def request_dispatcher(tasks: list[Task]) -> list[Answer]:
+    async with aiohttp.ClientSession(headers=base_headers) as session:
+        pack = [asyncio.ensure_future(get_request(session, task)) for task in tasks]
+        # TODO: make request from each proxy
+        for task in tasks:
             pack.append(asyncio.ensure_future(my_request(session, task)))
-
-    answers = await asyncio.gather(*pack)
-    return answers
+        answers = await asyncio.gather(*pack)
+        return answers
 
 
 async def my_request(session, task: Task):
     start_time = time.time()
     status = -1
     try:
-        if task.method == "get":
+        if task.request_type == "GET":
             status = await get_request(session, task)
     except Exception as e:
         logging.error(e)
 
     end_time = time.time()
-    if status == task.expectation_code:
+    if status == task.expectation:
         if end_time - start_time > 0.5:
-            return Answer(db_id=task.db_id, status="partial")
-        return Answer(db_id=task.db_id, status="ok")
-    return Answer(db_id=task.db_id, status="critical")
+            return Answer(uuid=task.uuid, status="partial")
+        return Answer(uuid=task.uuid, status="ok")
+    return Answer(uuid=task.uuid, status="critical")
 
 
 async def get_request(session, task: Task) -> Answer:
