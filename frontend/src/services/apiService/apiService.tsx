43ff79f9d817abf946ede2640aac2e111b70b90e
sanyuchilas <sanyuchilas@gmail.com>
2023-02-20 18:11:29 +0300

    hardcode baseurl

--- frontend/src/services/apiService/apiService.tsx
+++ frontend/src/services/apiService/apiService.tsx
@@ -1,11 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { GetCheckResultsRequestTypes, GetCheckResultsResponseTypes, ReportRequestTypes, ReviewGetTypes, ReviewRequestTypes, SocialReporGetTypes, SourceGetRequestTypes, SourceGetTypes, SubscriptionGetResponseTypes, SubscriptionPatchTypes, SubscriptionPostResponseTypes, SubscriptionPostTypes, UserLoginResponseTypes, UserRegistrRequestTypes, UserRegistrResponseTypes } from './apiServiceTypes'
 
+const baseUrl = `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/api/`
+
 class CreateRequest {
   public method: string;
   constructor (public url: string, public body: string | FormData, public headers?: {[key: string]: string | undefined}) {
     this.method = 'POST'
-    this.url = url;
+    this.url = baseUrl + url;
     this.body = body;
     this.headers = {'Content-Type': 'application/json', ...headers}
   }
@@ -14,7 +16,6 @@ class CreateRequest {
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/api/`,
     prepareHeaders(headers, {getState}) {
       const token = localStorage.getItem('token')
 
@@ -28,60 +29,56 @@ export const api = createApi({
   tagTypes: ['User', 'Source'],
   endpoints: (builder) => ({
     registerUser: builder.mutation<UserRegistrResponseTypes, UserRegistrRequestTypes>({
-      query: (user) => new CreateRequest('auth/users', JSON.stringify(user))
+      query: (user) => new CreateRequest('auth/users/', JSON.stringify(user))
     }),
     loginUser: builder.mutation<UserLoginResponseTypes, FormData>({
       query: (user) => new CreateRequest('auth/login/access-token', user, {'Content-Type': undefined})
     }),
     checkUser: builder.mutation<UserRegistrResponseTypes, void>({
-      query: () => ({url: 'auth/users/me'})
+      query: () => ({url: baseUrl + 'auth/users/me'})
     }),
     getSource: builder.query<SourceGetTypes, string>({
-      query: (sourceUuid) => ({ url: `resources/${sourceUuid}` })
+      query: (sourceUuid) => ({ url: baseUrl + `resources/${sourceUuid}` })
     }),
     getAllSources: builder.query<SourceGetTypes[], SourceGetRequestTypes>({
-      query: (params) => {
-        console.log(`${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/api/`)
-
-        return ({ 
-          url: `resources`,
-          params
-        })
-      }
+      query: (params) => ({ 
+        url: baseUrl + `resources`,
+        params
+      })
     }),
     getAllSocialReports: builder.query<SocialReporGetTypes[], string>({
-      query: (sourceUuid) => ({ url: `resources/${sourceUuid}/social_reports`})
+      query: (sourceUuid) => ({ url: baseUrl + `resources/${sourceUuid}/social_reports`})
     }),
     getAllReviews: builder.query<ReviewGetTypes[], string>({
-      query: (sourceUuid) => ({ url: `resources/${sourceUuid}/reviews` })
+      query: (sourceUuid) => ({ url: baseUrl + `resources/${sourceUuid}/reviews` })
     }),
     postSubscriptions: builder.mutation<SubscriptionPostResponseTypes, SubscriptionPostTypes>({
-      query: (subs) => new CreateRequest('subscriptions', JSON.stringify(subs))
+      query: (subs) => new CreateRequest('subscriptions/', JSON.stringify(subs))
     }),
     patchSubscriptions: builder.mutation<void, SubscriptionPatchTypes>({
       query: ({uuid, ...subs}) => ({
         method: 'PATCH',
-        url: `subscriptions/${uuid}`,
+        url: baseUrl + `subscriptions/${uuid}`,
         params: {...subs},
         headers: {'Content-Type': 'application/json'}
       })
     }),
     getSubscriptions: builder.query<SubscriptionGetResponseTypes[], string>({
       query: (resource_id) => ({ 
-        url: `auth/users/me/subscriptions`,
+        url: baseUrl + `auth/users/me/subscriptions`,
         params: {resource_id}
       })
     }),
     postReview: builder.mutation<void, ReviewRequestTypes>({
-      query: (review) => new CreateRequest('reviews', JSON.stringify(review))
+      query: (review) => new CreateRequest('reviews/', JSON.stringify(review))
     }),
     postReport: builder.mutation<void, ReportRequestTypes>({
-      query: (report) => new CreateRequest('reports', JSON.stringify(report))
+      query: (report) => new CreateRequest('reports/', JSON.stringify(report))
     }),
     getAllCheckResults: builder.query<GetCheckResultsResponseTypes[], GetCheckResultsRequestTypes>({
       query: ({source_uuid, ...params}) => ({
         params,
-        url: `resources/${source_uuid}/stats/checks`
+        url: baseUrl + `resources/${source_uuid}/stats/checks`
       })
     }),
   })
