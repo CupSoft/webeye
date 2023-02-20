726de3dd5b371313e47e6a56222cc574fed91bbd
Artem <artem00561@gmail.com>
2023-02-20 19:46:36 +0300

    fix: fix URLS
    Took 5 minutes

--- bot/app/services/restapi/URLS.py
+++ bot/app/services/restapi/URLS.py
@@ -1,7 +1,7 @@
 from app.settings import settings as s
 
-BASE_URL = f"{s().API_HOST}:{s().API_PORT}/api/"
-URL_GET_JWT = f"{BASE_URL}auth/users/telegram/get_jwt/"
-URL_GET_MY_RESOURCES_UUIDS = f"{BASE_URL}auth/users/me/subscriptions/"
-URL_GET_RESOURCE = f"{BASE_URL}resources/"
-URL_DELETE_USER = f"{BASE_URL}auth/users/me/tg_id/"
+BASE_URL = f"{s().API_HOST}:{s().API_PORT}/api"
+URL_GET_JWT = f"{BASE_URL}/auth/users/telegram/get_jwt"
+URL_GET_MY_RESOURCES_UUIDS = f"{BASE_URL}/auth/users/me/subscriptions"
+URL_GET_RESOURCE = f"{BASE_URL}/resources"
+URL_DELETE_USER = f"{BASE_URL}/auth/users/me/tg_id"
