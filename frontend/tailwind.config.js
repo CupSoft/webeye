2af53afb59b9df0ac55d04b9b1350fc98453b0da
sanyuchilas <sanyuchilas@gmail.com>
2023-03-18 01:11:29 +0300

    feat(frontend): redesign frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,20 +6,27 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'purple': '#8390ca',
+        // 'purple': '#8390ca',
+        'purple': '#7214FF',
         'purple-dark': '#6004EE',
         'green': '#41E88D',
         'red': '#FF5367',
         'gray-light': '#8f9bb773',
-        'blue': '#657BDB',
+        // 'blue': '#657BDB',
+        'blue': '#2d55fb',
         'gray-dark': '#16213A',
-        'gray-card': '#1B253E',
-        'gray-input': '#212D48',
-        'focus-input': '#2C3854',
+        // 'gray-card': '#1B253E',
+        'gray-card': '#0E1330',
+        // 'gray-input': '#212D48',
+        'gray-input': '#11152F',
+        // 'focus-input': '#2C3854',
+        'focus-input': '#0E1330',
         'light-gray-card': '#282D45',
         'light-text': '#949EB5',
-        'turquoise': '#48A3D6',
-        'yellow': '#FBFE72'
+        // 'turquoise': '#48A3D6',
+        'turquoise': '#FFF',
+        // 'yellow': '#FBFE72'
+        'yellow': '#FFF'
       },
       transitionTimingFunction: {
         DEFAULT: 'ease-in-out'
